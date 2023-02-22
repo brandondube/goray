638b83d4c53566f7a8fae10635f239cda1a18d04
Brandon Dube <brandon@retrorefractions.com>
2023-02-21 21:29:08 -0800

    thinfilm: add parallel test

--- thinfilm_test.go
+++ thinfilm_test.go
@@ -3,6 +3,7 @@ package goray
 import (
 	"math"
 	"math/cmplx"
+	"sync"
 	"testing"
 )
 
@@ -70,6 +71,41 @@ func BenchmarkThinFilmMultilayerAR(b *testing.B) {
 	var r complex128
 	for i := 0; i < b.N; i++ {
 		r, _ = MultilayerStackrt(Ppol, lambda, stack, 0, true)
+	}
+	_ = r
+}
+
+func BenchmarkThinFilmMultilayerARParallel8Core(b *testing.B) {
+	const (
+		lambda  = .587725
+		n_C7980 = 1.458461
+		n_MgF2  = 1.3698
+		n_CeF3  = complex(1.6290, 0.0034836)
+		n_ZrO2  = 2.1588
+
+		NTHREADS = 8
+		cases    = 100 * 100
+	)
+	stack := []NT{
+		{N: n_MgF2, T: lambda / 4},
+		{N: n_ZrO2, T: lambda / 2},
+		{N: n_CeF3, T: lambda / 4},
+		{N: n_C7980, T: 10_000},
+	}
+	var r complex128
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		for i := 0; i < NTHREADS; i++ {
+			wg.Add(1)
+			go func() {
+				for j := 0; j < (cases / NTHREADS); j++ {
+					MultilayerStackrt(Ppol, lambda, stack, 0, true)
+				}
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		// r, _ = MultilayerStackrt(Ppol, lambda, stack, 0, true)
 	}
 	_ = r
 }
